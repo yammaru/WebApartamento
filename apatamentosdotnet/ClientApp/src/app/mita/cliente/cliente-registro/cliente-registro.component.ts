9a67046a7d1e7a601910d85811e9793bf3df7da9
yammaru <63098405+yammaru@users.noreply.github.com>
2021-05-13 13:57:54 -0500

    mociminetos registro html

--- apatamentosdotnet/ClientApp/src/app/mita/cliente/cliente-registro/cliente-registro.component.ts
+++ apatamentosdotnet/ClientApp/src/app/mita/cliente/cliente-registro/cliente-registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { Cliente } from '../../models/cliente';
 
 @Component({
   selector: 'app-cliente-registro',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cliente-registro.component.css']
 })
 export class ClienteRegistroComponent implements OnInit {
+cliente: Cliente;
+  formGroup: FormGroup;
 
-  constructor() { }
+  constructor(private clienteService: ClienteService,private formBuilder: FormBuilder) { }
+   ngOnInit() {
+    this.cliente=new Cliente();
+        this.buildForm();
+      }
+   
+    private buildForm() {
+      this.cliente=new Cliente();
+      this.cliente.idCliente='';
+      this.cliente.nombre='';
+      this.cliente.telefono='';
+       
 
-  ngOnInit() {
+          this.formGroup = this.formBuilder.group({
+        idCliente: [this.cliente.idCliente, Validators.required],
+        Nombre: [ this.cliente.nombre, Validators.required],
+        telofono: [this.cliente.telefono,Validators.required],
+            });
+        }
+      get control() { 
+        return this.formGroup.controls;
+         }
+        
+        onSubmit() {
+              if (this.formGroup.invalid) {
+                return;
+              }
+              this.add();
+            }
+          
+  add() {
+    this.cliente = this.formGroup.value;
+
+    this.clienteService.post(this.cliente).subscribe(p => {
+      if (p != null) {
+        alert('Registro Completado!');
+        this.cliente = p;
+      }
+    });
+
   }
 
 }
--- apatamentosdotnet/ClientApp/src/app/mita/movimiento/movimiento-registro/movimiento-registro.component.ts
+++ apatamentosdotnet/ClientApp/src/app/mita/movimiento/movimiento-registro/movimiento-registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MovimientoService } from 'src/app/services/movimiento.service';
+import { Movimiento } from '../../models/movimiento';
 
 @Component({
   selector: 'app-movimiento-registro',
@@ -6,10 +9,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movimiento-registro.component.css']
 })
 export class MovimientoRegistroComponent implements OnInit {
+ movimiento: Movimiento;
+  formGroup: FormGroup;
 
-  constructor() { }
+  constructor(private movimientoService: MovimientoService,private formBuilder: FormBuilder) { }
+   ngOnInit() {
+    this.movimiento=new Movimiento();
+        this.buildForm();
+      }
+   
+    private buildForm() {
+      this.movimiento=new Movimiento();
+      this.movimiento.IdMovimiento='';
+      this.movimiento.Detalle='';
+      this.movimiento.IdUsuario='';
+      this.movimiento.Fecha;
+      this.movimiento.valor=0;
+       
 
-  ngOnInit() {
+          this.formGroup = this.formBuilder.group({
+        idMovimiento: [this.movimiento.IdMovimiento, Validators.required],
+        detalle: [ this.movimiento.Detalle, Validators.required],
+        idUsuario: [this.movimiento.IdUsuario,Validators.required],
+        fecha:[this.movimiento.Fecha,Validators.required],
+        valor:[this.movimiento.valor,Validators.required]
+            });
+        }
+      get control() { 
+        return this.formGroup.controls;
+         }
+        
+        onSubmit() {
+              if (this.formGroup.invalid) {
+                return;
+              }
+              this.add();
+            }
+          
+  add() {
+    this.movimiento = this.formGroup.value;
+
+    this.movimientoService.post(this.movimiento).subscribe(p => {
+      if (p != null) {
+        alert('Registro Completado!');
+        this.movimiento = p;
+      }
+    });
+
   }
+
 
 }
