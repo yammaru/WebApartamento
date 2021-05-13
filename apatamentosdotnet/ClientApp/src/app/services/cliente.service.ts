b22df5d78ebcc3374dc39c6b2d8219be3c259c6b
yammaru <63098405+yammaru@users.noreply.github.com>
2021-05-13 12:17:28 -0500

    cliente y usuario service

--- apatamentosdotnet/ClientApp/src/app/services/cliente.service.ts
+++ apatamentosdotnet/ClientApp/src/app/services/cliente.service.ts
@@ -1,9 +1,42 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { catchError,tap } from 'rxjs/operators';
+import { Observable,of } from 'rxjs';
+import { HandleHttpErrorService } from '../@base/handle-http-error.service';
+import { Cliente } from '../mita/models/cliente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
-
-  constructor() { }
+  baseUrl: string;
+  constructor(
+  private http: HttpClient,
+  @Inject('BASE_URL') baseUrl: string,
+  private handleErrorService: HandleHttpErrorService)
+  { 
+  this.baseUrl = baseUrl;
+  }
+  
+  post(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.baseUrl + 'api/Cliente',cliente)
+    .pipe(
+    tap(_ => this.handleErrorService.log('datos enviados')),
+    catchError(this.handleErrorService.handleError<Cliente>('Registrar Cliente', null))
+    );
+    }
+    buscar(id:string):Observable<Cliente> {
+      return this.http.post<Cliente>(this.baseUrl + 'api/Cliente/5', id)
+      .pipe(
+      tap(_ => this.handleErrorService.log('datos enviados')),
+      catchError(this.handleErrorService.handleError<Cliente>('Encontrar Cliente', null))
+      );
+    }
+    get(): Observable<Cliente[]> {
+      return this.http.get<Cliente[]>(this.baseUrl + 'api/Cliente')
+      .pipe(
+      tap(_ => this.handleErrorService.log('datos enviados')),
+      catchError(this.handleErrorService.handleError<Cliente[]>('Consulta Cliente', null))
+      );
+      }
 }
--- apatamentosdotnet/ClientApp/src/app/services/usuario.service.ts
+++ apatamentosdotnet/ClientApp/src/app/services/usuario.service.ts
@@ -1,9 +1,54 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { catchError,tap } from 'rxjs/operators';
+import { Observable,of } from 'rxjs';
+import { HandleHttpErrorService } from '../@base/handle-http-error.service';
+import { Usuario } from '../mita/models/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
-
-  constructor() { }
+  baseUrl: string;
+  constructor(
+  private http: HttpClient,
+  @Inject('BASE_URL') baseUrl: string,
+  private handleErrorService: HandleHttpErrorService)
+  { 
+    
+  this.baseUrl = baseUrl;
+  }
+  post(usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>(this.baseUrl + 'api/Usuario',usuario)
+    .pipe(
+    tap(_ => this.handleErrorService.log('datos enviados')),
+    catchError(this.handleErrorService.handleError<Usuario>('Registrar Usuario', null))
+    );
+    }
+    buscar(id:string):Observable<Usuario> {
+      return this.http.post<Usuario>(this.baseUrl + 'api/Usuario/5', id)
+      .pipe(
+      tap(_ => this.handleErrorService.log('datos enviados')),
+      catchError(this.handleErrorService.handleError<Usuario>('Encontrar Usuario', null))
+      );
+    }
+    get(): Observable<Usuario[]> {
+      return this.http.get<Usuario[]>(this.baseUrl + 'api/Usuario')
+      .pipe(
+      tap(_ => this.handleErrorService.log('datos enviados')),
+      catchError(this.handleErrorService.handleError<Usuario[]>('Consulta Usuario', null))
+      );
+      }
+    /*  Cambiar(): Observable<Usuario[]>{return this.http.put<Usuario[]>(this.baseUrl + 'api/Usuario/')
+      .pipe(
+      tap(_ => this.handleErrorService.log('datos enviados')),
+      catchError(this.handleErrorService.handleError<Usuario[]>('Consulta Usuario', null))
+      );
+      }*/
+     Eliminar(): Observable<Usuario[]>{return this.http.delete<Usuario[]>(this.baseUrl + 'api/Usuario/5')
+     .pipe(
+     tap(_ => this.handleErrorService.log('datos enviados')),
+     catchError(this.handleErrorService.handleError<Usuario[]>('Consulta Usuario', null))
+     );
+     }
 }
